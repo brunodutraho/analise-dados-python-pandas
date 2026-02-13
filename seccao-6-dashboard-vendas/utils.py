1e3b8c6d6d3afe8f5c6c13986ba2c9a43152ca59
Bruno Dutra <brunodutra.ho@gmail.com>
2026-02-13 13:08:45 -0300

    feat: implementa utilitário global de carregamento de CSS e melhorias na formatação numérica

--- seccao-6-dashboard-vendas/utils.py
+++ seccao-6-dashboard-vendas/utils.py
@@ -1,3 +1,14 @@
+# Leitura dos arquivos CSS
+import os
+def load_all_css(folder_path="styles"):
+    css = ""
+    for file in os.listdir(folder_path):
+        if file.endswith(".css"):
+            with open(os.path.join(folder_path, file), encoding="utf-8") as f:
+                css += f.read()
+    return f"<style>{css}</style>"
+
+# Formatação de valores monetários    
 def format_number(value, prefix = ''):
     for unit in ['', 'mil']:
         if value < 1000:
