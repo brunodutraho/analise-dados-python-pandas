b4f694e467c2499529c71d5f5868fd63843eef2c
Bruno Dutra <brunodutra.ho@gmail.com>
2026-02-13 10:14:57 -0300

    feat(transformacoes): adiciona agregação de receita mensal com tratamento de datas

--- seccao-6-dashboard-vendas/transformacoes.py
+++ seccao-6-dashboard-vendas/transformacoes.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 @st.cache_data
 def receita_por_estado(df):
@@ -11,3 +12,20 @@ def receita_por_estado(df):
           )
           .sort_values('Preço', ascending=False)
     )
+
+
+@st.cache_data
+def receita_mensal(df):
+        df_temp = df.copy()
+        df_temp['Data da Compra'] = pd.to_datetime(df_temp['Data da Compra'])
+        df_mensal = ( 
+            df_temp.set_index('Data da Compra')
+            .groupby(pd.Grouper(freq='M'))['Preço']
+            .sum()
+            .reset_index()
+        )
+        df_mensal['Ano'] = df_mensal['Data da Compra'].dt.year
+        df_mensal['Mes'] = df_mensal['Data da Compra'].dt.month_name()
+    
+        return df_mensal
+
