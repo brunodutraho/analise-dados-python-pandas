ec461be6701690b2200af9cb424cacd964d30e7a
Bruno Dutra <brunodutra.ho@gmail.com>
2026-02-15 18:29:59 -0300

    feat: adiciona ranking de quantidade de vendas e padroniza uso de performance_vendedores

--- seccao-6-dashboard-vendas/app.py
+++ seccao-6-dashboard-vendas/app.py
@@ -5,14 +5,15 @@ from transformacoes import (
     receita_por_estado,
     receita_mensal,
     receita_por_categoria,
-    receita_por_vendedores
+    performance_vendedores
 )
 from graficos import (
     grafico_receita_estado,
     grafico_receita_mensal,
     grafico_barra_receita_estado,
     grafico_receita_categoria,
-    grafico_receita_vendedores
+    grafico_receita_vendedores,
+    grafico_vendas_vendedores
 )
 
 st.set_page_config(
@@ -28,7 +29,11 @@ st.divider()
 df_rec_estado = receita_por_estado(df)
 df_rec_mensal = receita_mensal(df)
 df_rec_categoria = receita_por_categoria(df)
-df_rec_vendedores = receita_por_vendedores(df)
+df_perf = performance_vendedores(df)
+
+df_rec_vendedores = df_perf.sort_values('Receita_Total', ascending=False)
+df_vendas_vendedores = df_perf.sort_values('Quantidade_Vendas', ascending=False)
+
 
 aba_dataset, aba_receita, aba_vendedores = st.tabs(
     ['\U0001F4C4 Dataset', '\U0001F4B0 Receita', '\U0001F464 Vendedores']
@@ -92,8 +97,15 @@ with aba_vendedores:
     coluna1, coluna2 = st.columns(2)
 
     with coluna1:
-        st.subheader("Receita por Vendedores")
+        st.subheader("Top 7 Receita por Vendedores")
         st.plotly_chart(
             grafico_receita_vendedores(df_rec_vendedores),
+            use_container_width=True
+        )
+    
+    with coluna2:
+        st.subheader("Top 7 Quantidade de Vendas por Vendedores")
+        st.plotly_chart(
+            grafico_vendas_vendedores(df_vendas_vendedores),
             use_container_width=True
         )
