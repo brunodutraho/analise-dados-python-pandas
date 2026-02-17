0260080c0ec6e8eb4b9b30b53c1db795cfe0aaa4
Bruno Dutra <brunodutra.ho@gmail.com>
2026-02-17 02:28:12 -0300

    refactor(dataset): aplicar cache_data e melhorar carregamento para deploy

--- seccao-6-dashboard-vendas/dataset.py
+++ seccao-6-dashboard-vendas/dataset.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import streamlit as st
 from pathlib import Path
 from core.validators import (
     validar_colunas_obrigatorias,
@@ -9,19 +10,20 @@ from core.validators import (
 BASE_DIR = Path(__file__).parent
 DATASET_PATH = BASE_DIR / 'datasets' / 'vendas.json'
 
-with open(DATASET_PATH, encoding='utf-8') as file:
-    data = json.load(file)
 
-df = pd.DataFrame(data)
+@st.cache_data
+def carregar_dados():
+    with open(DATASET_PATH, encoding='utf-8') as file:
+        data = json.load(file)
 
-df['Data da Compra'] = pd.to_datetime(
-    df['Data da Compra'],
-    format='%d/%m/%Y'
-)
+    df = pd.DataFrame(data)
 
-# ===============================
-# VALIDAÇÕES E TRATAMENTOS
-# ===============================
+    df['Data da Compra'] = pd.to_datetime(
+        df['Data da Compra'],
+        format='%d/%m/%Y'
+    )
 
-validar_colunas_obrigatorias(df)
-df = tratar_valores_nulos(df)
+    validar_colunas_obrigatorias(df)
+    df = tratar_valores_nulos(df)
+
+    return df
